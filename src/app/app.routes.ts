5172579d4542d68675f03999758972ed9ff25ff7
Guy-S-glitch <guysolomon365@gmail.com>
2025-05-17 14:02:10 +0300

    Added necessary components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './sections/about/about.component';
+import { ContactComponent } from './sections/contact/contact.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { EducationComponent } from './sections/education/education.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'projects',component:ProjectsComponent},
+    {path:'skills', component:SkillsComponent},
+    {path:'experience',component:ExperienceComponent},
+    {path:'education',component:EducationComponent},
+    {path:'',redirectTo:'/about',pathMatch:'full'},
+    {path:'**',redirectTo:'/about'},
+];
