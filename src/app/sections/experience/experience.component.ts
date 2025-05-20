e15c11019e2ed7d2a9df88b28999848f7c59c43f
Guy-S-glitch <guysolomon365@gmail.com>
2025-05-20 22:52:30 +0300

    feat(experience): implement vertical timeline for experience section with images

--- src/app/sections/experience/experience.component.ts
+++ src/app/sections/experience/experience.component.ts
@@ -1,13 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css'],
-  standalone: true,
 })
-export class ExperienceComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class ExperienceComponent {
+  experienceEntries = [
+    {
+      year: '2024 - 2025',
+      title:
+        'Graduate, Pre-Military Academy “Chance to Change”, Beit Berl, Israel',
+      description: [
+        'Completed a year-long preparatory program focused on leadership, physical resilience, and national contribution.',
+        'Engaged in intensive physical training: treks, field weeks, navigation, night drills.',
+        'Participated in daily teamwork within a diverse and inclusive group.',
+        'Held responsibilities in logistics, cooking, and group coordination.',
+        'Contributed to civic engagement, ethical discussions, and community projects.',
+      ],
+      imageUrl: '../../assets/images/Chance_To_Change.png',
+    },
+    {
+      year: '2024 - 2025',
+      title: 'Wheelchair Technician, Kfar Saba, Israel',
+      description: [
+        'Provided technical support and repair services for mobility equipment.',
+        'Applied mechanical and electrical knowledge for diagnostics and maintenance.',
+        'Adapted solutions to specific user needs, improving comfort and reliability.',
+        'Developed practical problem-solving skills and customer service experience.',
+      ],
+      imageUrl: '../../assets/images/Will_To_Wheel.jpeg',
+    },
+    {
+      year: '2023 - 2024',
+      title: 'Reshef Technologies, Sderot, Israel',
+      description: [
+        'Performed electrical testing while maintaining strict safety protocols',
+        ' Demonstrated deep understanding of complex electronic systems ',
+        ' identified faults in electronic boards using a Fluke multimeter.',
+      ],
+      imageUrl: '../../assets/images/Reshef_Technologies.jpeg',
+    },
+    // Add more entries as needed
+  ];
 }
