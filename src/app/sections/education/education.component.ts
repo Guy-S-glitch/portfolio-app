0e5b801fea8fde71c986bc31d77545aef6a2e11c
Guy-S-glitch <guysolomon365@gmail.com>
2025-05-20 14:52:02 +0300

    feat(education): implement vertical timeline for education section with images

--- src/app/sections/education/education.component.ts
+++ src/app/sections/education/education.component.ts
@@ -1,13 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css'],
+  imports:[CommonModule],
   standalone: true,
 })
 export class EducationComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  educationEntries = [
+    {
+      year: '2022',
+      degree: 'Diploma – Technicians and Engineers',
+      institution: 'Ashkelon University',
+      description:
+        'Focused on software engineering, algorithms, and data structures.',
+      imageUrl: 'assets/images/Ashkelon.jpg',
+    },
+    {
+      year: '2018',
+      degree: 'High School Diploma',
+      institution: 'Technological certification certificate',
+      description: 'Majored in Mathematics and Physics.',
+      imageUrl: 'assets/images/Ashkelon-high.jpg',
+    },
+    // Add more entries as needed
+  ];
 }
